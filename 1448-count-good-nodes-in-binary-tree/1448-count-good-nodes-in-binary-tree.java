0bb7c000889a82846b9e011ccec02d231c92b5c1
srishti verma <143160172+srishtiverma235@users.noreply.github.com>
2026-07-29 15:03:33 +0530

    Time: 2 ms (93.5%), Space: 56.9 MB (69.04%) - LeetHub

---
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
@@ -0,0 +1,26 @@
+class Solution {
+
+    public int goodNodes(TreeNode root) {
+        return dfs(root, root.val);
+    }
+
+    private int dfs(TreeNode node, int maxSoFar) {
+
+        if (node == null) {
+            return 0;
+        }
+
+        int count = 0;
+
+        if (node.val >= maxSoFar) {
+            count++;
+        }
+
+        int newMax = Math.max(maxSoFar, node.val);
+
+        count += dfs(node.left, newMax);
+        count += dfs(node.right, newMax);
+
+        return count;
+    }
+}
