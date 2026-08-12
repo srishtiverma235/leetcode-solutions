d3210badfdafd14f4a57b4039ceccd1137cbd273
srishti verma <143160172+srishtiverma235@users.noreply.github.com>
2026-08-12 11:39:25 +0530

    Time: 0 ms (100%), Space: 46.6 MB (76.81%) - LeetHub

---
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int maxSum=Integer.MIN_VALUE;
+    public int maxPathSum(TreeNode root) {
+        maxGain(root);
+        return maxSum;
+        
+    }
+    private int maxGain(TreeNode root){
+        if (root==null)    
+            return 0;
+        int leftGain=Math.max(0,maxGain(root.left));
+        int rightGain=Math.max(0,maxGain(root.right));
+        int currentPath= leftGain+root.val+rightGain;
+        maxSum=Math.max(currentPath,maxSum);
+        return root.val+Math.max(leftGain,rightGain);
+        
+}
+}
