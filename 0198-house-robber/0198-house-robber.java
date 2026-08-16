429e1a60f7c83041242f1479c3c101a00c5840d6
srishti verma <143160172+srishtiverma235@users.noreply.github.com>
2026-08-16 14:01:00 +0530

    Time: 0 ms (100%), Space: 42.7 MB (75.71%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int rob(int[] nums) {
+     int prev1=0;
+     int prev2=0;
+     for(int money:nums){
+        int current=Math.max(prev1,money+prev2);
+        prev2=prev1;
+        prev1=current;
+     }
+        
+        return prev1;
+    }
+}
