a6a1f98e4850922cd764eb39bc4612c4dc8d418a
srishti verma <143160172+srishtiverma235@users.noreply.github.com>
2026-08-20 23:22:09 +0530

    Time: 11 ms (21.76%), Space: 52.3 MB (13.55%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,23 +1,22 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        boolean[][] rows = new boolean[9][9];
-        boolean[][] cols = new boolean[9][9];
-        boolean[][] boxes = new boolean[9][9];
-
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                if (board[i][j] != '.') {
-                    int num = board[i][j] - '1';
-                    int boxIndex = (i / 3) * 3 + (j / 3);
-
-                    if (rows[i][num] || cols[j][num] || boxes[boxIndex][num]) {
-                        return false;
-                    }
-
-                    rows[i][num] = cols[j][num] = boxes[boxIndex][num] = true;
-                }
+        HashSet<String> set=new HashSet<>();
+     for(int i = 0;i<9;i++){
+        for(int j =0;j<9;j++){
+            if(board[i][j] =='.'){
+                continue;
+            }
+            if(!set.add (board[i][j]+"in row"+i)){
+                return false;
+            }
+             if(!set.add (board[i][j]+"in col"+j)){
+                return false;
+            }
+             if(!set.add (board[i][j]+"in sb"+i/3+j/3)){
+                return false;
             }
         }
-        return true;
+     }
+     return true;
     }
 }
